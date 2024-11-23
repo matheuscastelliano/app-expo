e35b8b8ab671d48b1161152d48a75ed476925316
Matheus Castelliano <58199879+matheuscastelliano@users.noreply.github.com>
2024-11-23 12:36:22 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () =>{
+
+  return (
+
+    <view> 
+    
+      <text> Hello World </text>
+
+    </view>
+  );
+
+}
+
+export default HomePage;
